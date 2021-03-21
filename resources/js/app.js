dbbda680ae6362daa0453181b44a130383c418c4
NikitaMakovey <niks.finn@gmail.com>
2021-03-21 13:20:36 +0300

    Add start configurations

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import Main from './views/Main'
+createApp(Main).mount("#app")
---
+++ resources/js/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="display: flex">
+    <h2 style="align-self: center; display: flex; width: 100%">{{ `Hello, ${ new Date().getFullYear() }!` }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Main"
+  }
+</script>
+
+<style scoped>
+
+</style>
